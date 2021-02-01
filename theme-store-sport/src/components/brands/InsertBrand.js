67726a67adae6da00ad16bca69790be3cd69eb27
lytran <55661581+carotsusison1998@users.noreply.github.com>
2021-02-01 17:57:27 +0700

    ischage brand

--- theme-store-sport/src/components/brands/InsertBrand.js
+++ theme-store-sport/src/components/brands/InsertBrand.js
@@ -1,15 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 export default class InsertBrand extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name_brand: '',
+            description_brand: '',
+        };
+    }
+    isChange = (event) => {
+        this.setState({
+            name_brand: event.target.value
+        })
+    }
     render() {
         return (
             <>
                 <form ref={this.form}>
-                    <div className="form-group name_product">
-                        <label htmlFor="exampleInputEmail1">Tên sản phẩm</label>
+                    <div className="form-group name_brand">
+                        <label htmlFor="exampleInputEmail1">Tên thương hiệu</label>
                         <input type="text" onChange={(event)=>this.isChange(event)} className="form-control" placeholder="Tên sản phẩm" name="name_product" />
                     </div>
-                    <button type="reset" onClick={(event)=>this.onClickAddProduct(event)} className="btn btn-primary">Thêm mới</button>
+                    <div className="form-group description_brand">
+                        <label htmlFor="exampleInputEmail1">Mô tả thương hiệu</label>
+                        <CKEditor
+                            editor={ ClassicEditor }
+                            onReady={ editor => {
+                                // You can store the "editor" and use when it is needed.
+                                // console.log( 'Editor is ready to use!', editor );
+                            } }
+                            onChange={ ( event, editor ) => {
+                                const data = editor.getData();
+                                this.setState({
+                                    description_brand: data
+                                })
+                            } }
+                        />
+                    </div>
+                    <button type="reset" onClick="" className="btn btn-primary">Thêm mới</button>
                 </form>
             </>
         )
